f0b14800958ac12d29150063a1be38b1f7260258
Mac Cowell <mac@has100ideas.com>
2019-09-24 13:18:16 -0700

    fix: Index.html must be in gh-pages root dir

--- ts/bindings/__file_snapshots__/BlockContent.R
+++
@@ -1,5 +0,0 @@
-#' Union type for valid block content.
-#'
-#' @export
-BlockContent = Union("CodeBlock", "CodeChunk", "Heading", "List", "ListItem", "Paragraph", "QuoteBlock", "Table", "ThematicBreak")
-
--- ts/bindings/__file_snapshots__/BlockContent.py
+++
@@ -1,4 +0,0 @@
-"""
-Union type for valid block content.
-"""
-BlockContent = Union["CodeBlock", "CodeChunk", "Heading", "List", "ListItem", "Paragraph", "QuoteBlock", "Table", "ThematicBreak"]
